018a7abe590141fbd32c8cbdf22336f5773fb341
ElkinC21 <elkin.leo.cruz21@gmail.com>
2025-08-01 17:35:44 +0200

    push

---
+++ Elkin_Cruz_Fernando_Madrid_LabProgra2_LabHerencia/src/elkin_cruz_fernando_madrid_labprogra2_labherencia/Empleado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package elkin_cruz_fernando_madrid_labprogra2_labherencia;
+
+/**
+ *
+ * @author elkin
+ */
+public class Empleado {
+    
+}
