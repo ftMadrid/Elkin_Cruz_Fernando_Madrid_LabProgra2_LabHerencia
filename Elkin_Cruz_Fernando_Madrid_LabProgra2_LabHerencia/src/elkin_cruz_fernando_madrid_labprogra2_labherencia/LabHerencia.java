72058ee71ce5f6b028f0e32e9a363b14912eacbd
ftMadrid <fernandomadridcv04@gmail.com>
2025-08-01 08:23:38 -0600

    First Commit

---
+++ Elkin_Cruz_Fernando_Madrid_LabProgra2_LabHerencia/src/elkin_cruz_fernando_madrid_labprogra2_labherencia/LabHerencia.java
@@ -0,0 +1,11 @@
+package elkin_cruz_fernando_madrid_labprogra2_labherencia;
+
+public class LabHerencia {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        
+    }
+    
+}
